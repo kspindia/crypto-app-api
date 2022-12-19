8b164a4ceaebe7e4753028e576b139d6cbd1588a
kspindia <panwar.678758@gmail.com>
2022-12-19 17:12:49 +0530

    initial commit
    date: 19 Dec 2022

---
+++ src/components/Hero.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Hero.css'
+import crypto from '../assets/hero-img.png'
+function Hero() {
+  return (
+    <div className='hero'>
+        <div className='container'>
+          {/* left side */}
+          <div className='left'>
+            <p>Buy & Sell Crypto 24/7 using your retirement account</p>
+            <h1>Invest in Cryptocurrency with your IRA</h1>
+            <p>Buy & Sell, and store hundreds of cryptocurrencies </p>
+            <div className='input-container'>
+             <input type='email' placeholder='Enter your email'/>
+             <button className='btn'>Learn More</button>
+            </div>
+          </div>
+          {/* right side */}
+          <div className='right'>
+           <div className='img-container'>
+            <img src={crypto} alt=''/>
+           </div>
+          </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
